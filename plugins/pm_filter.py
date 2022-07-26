1ca3f17f53b8c7f2ec8d6e1c8f235f4de759279d
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2022-07-26 20:38:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -153,7 +153,7 @@ async def give_filter(client,message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("😁 𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱,𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿𝘀𝗲𝗹𝗳.", show_alert=True)
+        return await query.answer("😁 𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱,𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿𝘀𝗲𝗹𝗳 സ്വന്തമായി റിക്വസ്റ്റ് ചെയ്യൂ,കണ്ടവരുടെ മുത്തലിൽ തുടരുത് ✋✋.", show_alert=True)
     try:
         offset = int(offset)
     except:

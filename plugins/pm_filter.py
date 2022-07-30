897b3bdd6344d89c71f643db316f757e001442f9
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2022-07-30 08:44:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -897,13 +897,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="▣ ▢ ▢"
+            text="🅻︎🅾︎🅳︎"
         )
         await query.message.edit_text(
-            text="▣ ▣ ▢"
+            text="🅻︎🅾︎🅰︎🅳︎🅸︎🅽︎🅶︎"
         )
         await query.message.edit_text(
-            text="▣ ▣ ▣"
+            text="🅻︎🅾︎🅰︎🅳︎🅸︎🅽︎🅶︎..."
         ) 
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),

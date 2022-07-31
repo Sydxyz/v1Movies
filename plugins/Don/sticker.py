d5a9c7ad25c6b8c8a6ef170bf025ccc10fc6203c
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2022-07-31 13:12:47 +0530

    Update sticker.py

--- plugins/Don/sticker.py
+++ plugins/Don/sticker.py
@@ -9,4 +9,4 @@ async def stickerid(bot, message):
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`\n\n ➕➕➕🅲︎🆁︎🅴︎🅳︎🅸︎🆃︎🆂︎➖➖➖ \n [𝙼𝙾𝙳 𝙼𝙾𝚅𝙸𝙴𝚉𝚇 📺](https://t.me/Mod_Moviezx) \n [𝙱𝙾𝚃 𝙲𝚁𝙰𝙲𝙺𝙴𝚁🧨🧨](https://t.me/bot_cracker) \n [𝙶𝚁𝙾𝚄𝙿🥨🥨🥨](https://t.me/malayalam_requester_bot)", quote=True)
     else: 
-       await message.reply("Oops !! Not a sticker file")
+       await message.reply("Oops !!That is Not a sticker file")
